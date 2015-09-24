de35059138440bf1cbf31bf1ebdb58cfd9bf2741
Atiq Sayyed <sayyedatiq@gmail.com>
2015-09-24 10:53:18 +0530

    Added Skeleton and basic script for running the test cases

---
+++ project/Build.scala
@@ -0,0 +1,13 @@
+import Dependencies._
+import sbt.Keys._
+import sbt._
+
+object Build extends Build {
+
+  val appName = "grinder"
+
+  val main = Project(appName, file(".")).settings(
+    (testOptions in Test) +=Tests.Argument(TestFrameworks.ScalaTest, "-h", "target/test-reports"),
+    libraryDependencies ++= dependencies()
+  )
+}
---
+++ src/test/scala/utils/Driver.scala
@@ -0,0 +1,18 @@
+package utils
+
+import java.util.concurrent.TimeUnit
+
+import org.openqa.selenium.WebDriver
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.FunSpec
+import org.scalatest.{BeforeAndAfterAll, FunSpec}
+import org.scalatest.selenium.WebBrowser
+
+
+trait Driver  extends FunSpec with BeforeAndAfterAll with WebBrowser {
+  implicit def  driver: WebDriver
+
+  def setTimeout() {driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS)}
+
+  override protected def afterAll = driver.close()
+}

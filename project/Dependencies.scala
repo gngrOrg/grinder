aaaa05aea4971e892baeeaba9c46ef09e8051c73
hrj <harshad.rj@gmail.com>
2015-09-25 13:43:35 +0530

    update to latest sbt, scala, dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,9 @@ object Dependencies {
   def dependencies() = {
     Seq(
       "org.seleniumhq.selenium" % "selenium-java" % "2.45.0",
-      "org.scalatest" % "scalatest_2.10" % "2.2.4" % "test->*",
-      "org.pegdown" % "pegdown" % "1.4.2" % "test"
+      "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test->*",
+      "org.scala-lang.modules" %% "scala-xml" % "1.0.5"
+      // "org.pegdown" % "pegdown" % "1.4.2" % "test"
     )
   }
 }

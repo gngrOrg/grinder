b45d356c209deaa0e4f693d690317ae23d8ea1b7
hrj <harshad.rj@gmail.com>
2015-09-27 15:38:07 +0530

    upgraded selenium

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   def dependencies() = {
     Seq(
-      "org.seleniumhq.selenium" % "selenium-java" % "2.45.0",
+      "org.seleniumhq.selenium" % "selenium-java" % "2.47.2",
       "org.scalatest" % "scalatest_2.11" % "2.2.5",
       "org.scala-lang.modules" %% "scala-xml" % "1.0.5"
       // "org.pegdown" % "pegdown" % "1.4.2" % "test"

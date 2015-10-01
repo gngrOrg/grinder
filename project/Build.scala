b2c2ead06333decc4edfdfd01f7a59f0d02a9418
hrj <harshad.rj@gmail.com>
2015-10-01 22:10:30 +0530

    scalac options to optimise compilation

--- project/Build.scala
+++ project/Build.scala
@@ -9,6 +9,7 @@ object Build extends Build {
   val main = Project(appName, file(".")).settings(
     // (testOptions in Test) +=Tests.Argument(TestFrameworks.ScalaTest, "-h", "target/test-reports"),
     resolvers += Resolver.sonatypeRepo("releases"),
-    libraryDependencies ++= dependencies()
+    libraryDependencies ++= dependencies(),
+    scalacOptions ++= Seq("-optimise", "-Yclosure-elim", "-Yinline")
   )
 }

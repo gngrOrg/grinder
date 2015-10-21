dbc691271892cd032560a7b5e4f1d66fac5286c9
hrj <harshad.rj@gmail.com>
2015-10-21 17:22:47 +0530

    moved result parsing to separate class

--- src/main/scala/grinder/UpdateBaseLine.scala
+++ src/main/scala/grinder/UpdateBaseLine.scala
@@ -5,22 +5,6 @@ import collection.JavaConversions._
 import collection.JavaConverters._
 
 object UpdateBaseLine {
-  private case class Result(pass: String, id: String)
-
-  import rapture.json._
-  import jsonBackends.jawn._
-
-  private def json(path: Path) = {
-    val resultPath = path.resolve("results.json")
-    val str = Files.readAllLines(resultPath).asScala.mkString("")
-    Json.parse(str)
-  }
-
-  private def getResults(path: Path) = {
-    val myJSON = json(path)
-    val tests = myJSON.selectDynamic("css21-reftests")
-    tests.results.as[Array[Result]]
-  }
 
   def update(args: Array[String]) {
     val newPath = Paths.get(args(0)).toAbsolutePath().normalize()
@@ -28,10 +12,10 @@ object UpdateBaseLine {
     println("New path: " + newPath)
     println("Base path: " + basePath)
 
-    val newResults = getResults(newPath)
+    val newResults = ResultParser.getResults(newPath)
     val (newPassed, newFailed) = newResults.partition(_.pass == "true")
 
-    val baseResults = getResults(basePath)
+    val baseResults = ResultParser.getResults(basePath)
     val (basePassed, baseFailed) = baseResults.partition(_.pass == "true")
     println(s"[base] passed: ${basePassed.length},  failed: ${baseFailed.length}")
     println(s"[new] passed: ${newPassed.length} (${newPassed.length - basePassed.length}),  failed: ${newFailed.length} (${newFailed.length - baseFailed.length})")
@@ -56,7 +40,7 @@ object UpdateBaseLine {
       val regressions = newFailed.filterNot(n => baseFailed.contains(n))
       println("Regressions: " + regressions.length)
       if (regressions.length > 0) {
-        var copied = Seq[Result]()
+        var copied = Seq[ResultParser.TestResult]()
         var continue = true
         var i = 0
         while (continue && i < regressions.length) {

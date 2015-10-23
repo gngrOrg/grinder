07b8360e2403064e9f69e9bb8747cda3209153ae
hrj <harshad.rj@gmail.com>
2015-10-23 21:32:19 +0530

    made dependencies intransitive

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,10 +7,14 @@ object Dependencies {
       "org.spire-math" %% "jawn-ast" % "0.8.3",
       "com.propensive" %% "rapture-json" % "2.0.0-M1",
       "com.propensive" %% "rapture-json-jawn" % "2.0.0-M1",
-      "org.seleniumhq.selenium" % "selenium-java" % "2.48.2",
+      "org.seleniumhq.selenium" % "selenium-java" % "2.48.2" intransitive(),
+      "org.seleniumhq.selenium" % "selenium-firefox-driver" % "2.48.2",
       "me.tongfei" % "progressbar_2.11" % "0.3.2",
       "org.scalatest" % "scalatest_2.11" % "2.2.5",
-      "org.scala-lang.modules" %% "scala-xml" % "1.0.5"
+      "org.scala-lang.modules" %% "scala-xml" % "1.0.5",
+
+      // Added for imgur upload, could be removed later
+      "org.apache.httpcomponents" % "httpmime" % "4.5.1"
     )
   }
 }
